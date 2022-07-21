005c3842fb146f33aae30d3b06c45d030799f27b
nandamsivaram <84092480+nandamsivaram@users.noreply.github.com>
2022-07-21 22:09:17 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -12,7 +12,7 @@ TEST_CASE("Test checkAndAlert functionality")
  BatteryCharacter batteryChar;
  batteryChar.coolingType = PASSIVE_COOLING;
   REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,-5) == 1);
-  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,15) == 0);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,20) == 0);
   REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,40) == 1);
   
   batteryChar.coolingType = HI_ACTIVE_COOLING;
@@ -30,13 +30,13 @@ TEST_CASE("Test classifyTemperatureBreach functionality")
 { 
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) 	 	== 	TOO_LOW);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) 		== 	TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) 		== 	TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) 	== 	TOO_LOW);
 	
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) 	 	== 	TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) 		== 	TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) 		== 	TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) 	== 	TOO_HIGH);
 	
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) 	 	== 	NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) 	 	== 	NORMAL);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 25) 		== 	NORMAL);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) 		== 	NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) 	== 	NORMAL);
 }
